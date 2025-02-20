75d6cd6bb988471363803faf3a4c4eab1f1f93a5
Phillip Meyer <philllipmeyer6291@gmail.com>
2025-02-20 15:58:07 +0200

    Idea for final project for cookieclicker

---
+++ main01.py
@@ -0,0 +1,9 @@
+# Create a project using selenium to automate the cookie clicker game. The project should be able to click the big cookie and buy products. The project should have the following features:
+# The bot will win and the game will end when the following conditions are met:
+# The bot has bought 10 cursors.
+# The bot has bought 8 grandmas.
+# The bot has bought 6 farms.
+# The bot has bought 4 mines.
+# The bot has bought 2 factories.
+# Figure out how get check the id of either upgrades or the products and then if their class name is create upgrade enabled then click on them.
+# Also then maybe get something to print if clickable so like if I look at the products offering and see that the product is clickable then I can click on it.
